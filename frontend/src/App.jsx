303000b0730d51b3ba80106f2eb324d52168cb55
Moynul Islam <moynul2015@cseku.ac.bd>
2026-01-31 17:21:05 +0600

    ELT-5: setup the page layout in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header.jsx";
+import MainContent from "./components/MainContent/MainContent.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <div className="page-layout">
+      <Header />
+      <MainContent>
+      </MainContent>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;

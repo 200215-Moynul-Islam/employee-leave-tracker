c1e1672ac5c4e3a34a35404da2cda55cfc9daa95
Moynul Islam <moynul2015@cseku.ac.bd>
2026-01-31 17:28:52 +0600

    ELT-5: install react-toastify and set up App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,27 @@ import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import MainContent from "./components/MainContent/MainContent.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="page-layout">
       <Header />
-      <MainContent>
-      </MainContent>
+      <MainContent></MainContent>
       <Footer />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        draggable
+        theme="light"
+      />
     </div>
   );
 }

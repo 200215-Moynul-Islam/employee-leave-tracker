7b6d2584b066b6a27ef3a88134e6e95781ecd24b
Moynul Islam <moynul2015@cseku.ac.bd>
2026-02-03 06:54:00 +0600

    ELT-24: implement jwt token integration to call an endpoint

--- frontend/src/api/leave.service.js
+++ frontend/src/api/leave.service.js
@@ -5,18 +5,21 @@ const leaveService = {
   getAllLeaveRequests: async () => {
     return await httpClient("leaves", {
       method: "GET",
+      requireAuth: true,
     });
   },
 
   approveLeaveRequest: async (id, userId) => {
     return await httpClient(`leaves/${id}/approve?userId=${userId}`, {
       method: "PATCH",
+      requireAuth: true,
     });
   },
 
   rejectLeaveRequest: async (id, userId) => {
     return await httpClient(`leaves/${id}/reject?userId=${userId}`, {
       method: "PATCH",
+      requireAuth: true,
     });
   },
 
@@ -24,12 +27,14 @@ const leaveService = {
     return await httpClient("leaves", {
       method: "POST",
       body: leaveRequest,
+      requireAuth: true,
     });
   },
 
   deleteLeaveRequest: async (id, userId) => {
     return await httpClient(`leaves/${id}?userId=${userId}`, {
       method: "DELETE",
+      requireAuth: true,
     });
   },
 };
